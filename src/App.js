aa4eb87c57f7cd80fdbf0570f7463f84eefb076a
Ed Keric <ed.keric@gmail.com>
2021-02-12 14:37:10 +0100

    firsr commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+
+        <a href="/">SHOPPING CART</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        No rights reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
